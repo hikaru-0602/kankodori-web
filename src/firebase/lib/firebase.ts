78510472030045c15613b79c1a6acb682c3f1a42
Hikaru Saito <beretta0602@gmail.com>
2025-09-07 16:58:46 +0900

    remocon set

--- src/firebase/lib/firebase.ts
+++ src/firebase/lib/firebase.ts
@@ -4,6 +4,7 @@ import { initializeApp, getApps, getApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
 import { getStorage } from 'firebase/storage';
+import { getRemoteConfig } from 'firebase/remote-config';
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_API_KEY,
@@ -22,3 +23,4 @@ const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 export const storage = getStorage(app);
+export const remoteConfig = getRemoteConfig(app);
